9b13f800b3933e7c349f47925f498abed8c06587
SohailAsghar <Sohailkundi6651@gmail.com>
2025-02-18 11:39:40 +0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -12,3 +12,18 @@ gsap.from("#sub-heading"{
     ease: "power2.out",
     delay: 0.6
 });
+gsap.from("#main-heading", {
+  y: -50,
+  opacity: 0,
+  duration: 1,
+  ease: "power2.out",
+  delay: 0.5
+});
+
+gsap.from("#sub-heading", {  // Added comma here
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  ease: "power2.out",
+  delay: 0.6
+});
