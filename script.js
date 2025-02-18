cec0ea52466252c991dae1b68bb312b4cfb81bf9
SohailAsghar <Sohailkundi6651@gmail.com>
2025-02-18 12:00:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// gsap.from("#main-heading", {
-//       y: -50,
-//       opacity: 0,
-//       duration: 1,
-//       ease: "power2.out",
-//       delay: 0.5
-//     });
-// gsap.from("#sub-heading"{
-//   y: 50,
-//     opacity: 0,
-//     duration: 1,
-//     ease: "power2.out",
-//     delay: 0.6
-// });
+
 gsap.from("#main-heading", {
   y: -50,
   opacity: 0,
